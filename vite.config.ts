de8a3ed236e41fc40c6b9c727687fc6294a6410e
jhleee <ng0301@gmail.com>
2025-08-21 23:18:04 +0900

    github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig, loadEnv } from 'vite';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: '/squircle-progress-card/',
       define: {
       },
       resolve: {
